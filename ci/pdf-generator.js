e3c8b6b0343c9e12170647612fcf9ebfc2754063
Kashyap Patel <patel.kashyap226@gmail.com>
2020-07-19 18:41:07 -0300

    trying non protocol url for page.pdf

--- ci/pdf-generator.js
+++ ci/pdf-generator.js
@@ -1,33 +1,32 @@
 const path = require('path');
 const puppeteer = require('puppeteer');
 
-const addProtocol = (path) => `file:/${path}`;
-
-try {
-    (async function () {
+(async function () {
+    try {
         const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
 
         const indexFilePath = path.resolve(__dirname, '../build/index.html');
 
-        const finalIndexPath = addProtocol(indexFilePath);
-
-        console.log('finalIndexPath ', finalIndexPath);
-        await page.goto(finalIndexPath, { waitUntil: 'networkidle0' });
+        console.log('indexFilePath ', `file:/${indexFilePath}`);
+        await page.goto(`file:/${indexFilePath}`, {
+            waitUntil: 'networkidle0',
+        });
 
         const pdfFilePath = path.resolve(
             __dirname,
             '../build/Kashyap_Patel_Resume.pdf'
         );
 
-        const finalPdfPath = addProtocol(pdfFilePath);
+        console.log('pdfFilePath ', pdfFilePath);
 
-        console.log('finalPdfPath ', finalPdfPath);
-
-        await page.pdf({ path: finalPdfPath });
+        await page.pdf({ path: pdfFilePath });
 
         await browser.close();
-    })();
-} catch (err) {
-    console.error('Error occured while printing pdf. ', err);
-}
+    } catch (err) {
+        console.error(
+            'Error occured while printing pdf. ',
+            JSON.stringify(err)
+        );
+    }
+})();
